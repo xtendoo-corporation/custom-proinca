9a3a6c94fcce206490c18652f8cca0c99e12302c
Dani <dani.xtendoo@gmail.com>
2024-01-05 08:42:16 +0100

    [ADD] proinca_document_format

---
+++ proinca_document_format/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Proinca Document Format",
+    "summary": """Proinca Document Format""",
+    "version": "16.0.1.0.0",
+    "description": """Proinca Document Format""",
+    "author": "Daniel Dominguez",
+    "company": "Xtendoo",
+    "website": "http://xtendoo.es",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "account",
+    ],
+    "data": [
+        "views/invoice/invoice_document.xml",
+
+    ],
+    "installable": True,
+    "auto_install": False,
+}
