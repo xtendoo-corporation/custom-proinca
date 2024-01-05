9d7602743bfb5a43d3aa3d48b953a12a4211ca85
Dani <dani.xtendoo@gmail.com>
2024-01-05 13:45:12 +0100

    [ADD] proinca_contacts_filter

---
+++ proinca_contacts_filter/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Proinca Contacts Filter",
+    "summary": """Proinca Contacts Filter""",
+    "version": "16.0.1.0.0",
+    "description": """Proinca Contacts Filter""",
+    "author": "Daniel Dominguez",
+    "company": "Xtendoo",
+    "website": "http://xtendoo.es",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "account",
+        "web",
+    ],
+    "data": [
+        "security/security_groups.xml",
+
+    ],
+    "installable": True,
+    "auto_install": False,
+}
