df1f807db4c2ff9941a67e9f2458a570a0bf4379
manuelcalerosolis <manuelcalerosolis@gmail.com>
2024-01-31 13:28:10 +0100

    text small

--- proinca_document_format/models/account_move.py
+++ proinca_document_format/models/account_move.py
@@ -9,13 +9,13 @@ class AccountMove(models.Model):
     sale_order_id = fields.Many2one(
         string="Pedido de Venta",
         comodel_name="sale.order",
-        compute="_compute_sale_order_id",
         store=True,
     )
 
-    def _compute_sale_order_id(self):
-        for invoice in self:
-            if invoice.invoice_line_ids.sale_line_ids:
-                sale_order_id = self.mapped("invoice_line_ids.sale_line_ids.order_id")
-                if sale_order_id:
-                    invoice.sale_order_id = sale_order_id[0].id
+    @api.model
+    def write(self, vals):
+        if self.invoice_line_ids.sale_line_ids:
+            sale_order_id = self.mapped("invoice_line_ids.sale_line_ids.order_id")
+            if sale_order_id:
+                vals['sale_order_id'] = sale_order_id[0].id
+        return super().write(vals)
