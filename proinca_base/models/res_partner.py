bf006c9347d7b4acfe5f7f7449abee184260fb61
dani <dani.xtendoo@gmail.com>
2024-04-30 13:34:49 +0200

    [MOD] proinca_base. quitado partner_id, daba problemas de recursividad

--- proinca_base/models/res_partner.py
+++ proinca_base/models/res_partner.py
@@ -7,10 +7,6 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    partner_id = fields.Many2one(
-        'res.partner',
-        string='Alumno'
-    )
     is_tutor = fields.Boolean(
         string='Tutor',
     )
@@ -30,11 +26,3 @@ class ResPartner(models.Model):
         string='Pedidos de Venta',
     )
 
-    def action_open_student(self):
-        self.ensure_one()
-        action = self.env["ir.actions.act_window"]._for_xml_id("proinca_base.action_open_student")
-        action['domain'] = [('partner_id', '=', self.id), ('is_alumno', '=', True)]
-        action['context'] = {
-            'default_partner_id': self.id,
-        }
-        return action
